57a60dbd6bbaf542ded855a1afce276655803a48
Tony Höglund <tony.hoglund@gmail.com>
2022-04-08 08:02:23 +0200

    minor configuration changes

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -1,6 +1,8 @@
 if command -v exa >/dev/null; then
-  alias exa="exa --group-directories-first --git --ignore-glob='.DS_Store'";
+  alias exa="exa --group-directories-first --icons --ignore-glob='.DS_Store'";
   alias l="exa -1";
   alias ll="exa -lag";
   alias la="LC_COLLATE=C exa -la";
 fi
+
+# alias diff="diff --color"
