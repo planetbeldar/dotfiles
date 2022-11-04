b5b97c0d7953f3e96058f661addf1ab27c8f6cf9
Tony Höglund <tony.hoglund@gmail.com>
2022-11-04 15:56:38 +0100

    bottom: override src for current HEAD

--- modules/shell/bottom.nix
+++ modules/shell/bottom.nix
@@ -1,10 +1,26 @@
 { config, options, lib, pkgs, ... }:
 let
   inherit (lib) mkIf util;
-  inherit (pkgs) bottom;
+  # inherit (pkgs) bottom;
 
   cfg = config.modules.shell.bottom;
   configDir = config.dotfiles.configDir;
+  bottom = pkgs.bottom.overrideAttrs(drv: rec {
+    version = "2022-11-04";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "ClementTsang";
+      repo = drv.pname;
+      rev = "6f95aaee34f7f9bfd49a4ebabfdce8dff76e03c0";
+      sha256 = "sha256-MWyYMBu5JxLdJZSTKMa/uqYGUcj4KbfPJzDWdVob/VM=";
+    };
+
+    cargoDeps = drv.cargoDeps.overrideAttrs (lib.const {
+      inherit src;
+      outputHash = "sha256-AIPWqOW5p8AXfaH0hOucUTgP8aeuRS4vS5jEqEGtkRc=";
+      # outputHash = lib.fakeHash;
+    });
+  });
 in {
   options.modules.shell.bottom = { enable = util.mkBoolOpt false; };
 
