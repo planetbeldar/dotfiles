948634ebc067441bdc15f1a0cd9c0717b6f3f4f1
Tony Höglund <tony.hoglund@gmail.com>
2023-10-22 22:26:53 +0200

    feat: update darwin application sync script

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -1,7 +1,7 @@
 { options, config, lib, pkgs, ... }:
 let
   inherit (lib) mkIf mkForce;
-  inherit (pkgs) stdenv;
+  inherit (pkgs) stdenv rsync gnugrep;
 in {
   config = mkIf stdenv.isDarwin {
 
@@ -9,33 +9,34 @@ in {
       "cursr"
     ];
 
-    # Copy mac applications installed via Nix to $HOME so Spotlight can index them
+    # Sync mac applications installed via Nix to users home
     system.activationScripts.applications.text = mkForce (''
-      hashApp() {
-        path="$1/Contents/MacOS/"; shift
-        /usr/bin/find -s "$path" -perm +111 -type f -maxdepth 1 -exec sh -c "md5sum {} | cut -b-32" \; 2> /dev/null || true \
-          | md5 -r | cut -b-32
+      syncApp() {
+        local src="$1"
+        local dst="$2"
+        ${rsync}/bin/rsync -aLci "$src" "$dst" | ${gnugrep}/bin/grep -c ">f" || true
       }
 
-      mkdir -p $HOME/Applications/
-      echo "Looking for mac apps in ${config.system.build.applications}"
+      applications="${config.system.build.applications}"
+      home="${config.user.home}"
 
-      /usr/bin/find ${config.system.build.applications}/Applications -maxdepth 1 -type l |
-      while read app; do
-        name="$(basename "$app")"
-        src="$(/usr/bin/stat -f%Y "$app")"
-        dst="$HOME/Applications/$name"
-        hash1="$(hashApp "$src")"
-        hash2="$(hashApp "$dst")"
+      mkdir -p "$home/Applications/"
+      echo "Looking for mac apps in $applications"
 
-        printf " Found '$name'"
-        if [ "$hash1" != "$hash2" ]; then
-          printf " and its hash differs from the store, copying.."
-          $DRY_RUN_CMD rm -fr "$dst"
-          $DRY_RUN_CMD cp --archive -H --dereference "$src" $HOME/Applications
-          printf " done.\n"
+      find "$applications"/Applications/* -maxdepth 1 -type l |
+      while read -r app; do
+        name=$(basename "$app")
+        printf " Found %s" "$name"
+
+        src=$(/usr/bin/stat -f%Y "$app")
+        dst="$home/Applications/$name"
+
+        printf " - Synchronizing..\r"
+        changes=$(syncApp "$src" "$home/Applications/")
+        if [[ "$changes" -ne 0 ]]; then
+          printf "%-40s -> Updated %s file(s)\n" "$name" "$changes"
         else
-          printf " and it's up to date with the store.\n"
+          printf "%-40s -> Up to date\n" "$name"
         fi
       done
     '');
