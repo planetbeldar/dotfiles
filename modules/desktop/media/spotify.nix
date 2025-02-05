7c9b28a167693786e28d246ef401b0949bbfff11
Tony Höglund <tony.hoglund@gmail.com>
2025-02-05 09:47:11 +0100

    spotify: no need for mac overlay

--- modules/desktop/media/spotify.nix
+++ modules/desktop/media/spotify.nix
@@ -1,15 +1,13 @@
 { options, config, lib, inputs, pkgs, ... }:
 let
   inherit (lib) util mkIf mkMerge;
-  inherit (pkgs) stdenv;
+  inherit (pkgs) stdenv spotify;
 
-  spotify = if stdenv.isDarwin then pkgs.spotify-mac else pkgs.spotify;
   cfg = config.modules.desktop.media.spotify;
 in {
   options.modules.desktop.media.spotify = { enable = util.mkBoolOpt false; };
 
   config = mkIf cfg.enable {
-    nixpkgs.overlays = [ inputs.mac-overlay.overlays.spotify-mac ];
     environment.systemPackages = [ spotify ];
   };
 }
