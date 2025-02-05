c3668775e6fbce194ec4f0f3fbf7340ed4e28175
Tony Höglund <tony.hoglund@gmail.com>
2025-02-05 09:46:55 +0100

    pgadmin: no need for mac overlay

--- modules/desktop/db/pgadmin.nix
+++ modules/desktop/db/pgadmin.nix
@@ -2,17 +2,15 @@
 
 let
   inherit (lib) util mkIf mkMerge;
-  inherit (pkgs) stdenv;
+  inherit (pkgs) stdenv pgadmin4;
 
   cfg = config.modules.desktop.db.pgadmin;
-  pgadmin = if stdenv.isDarwin then pkgs.pgadmin4-mac else pkgs.pgadmin4;
 in {
   options.modules.desktop.db.pgadmin = {
     enable = util.mkBoolOpt false;
   };
 
   config = mkIf cfg.enable {
-    nixpkgs.overlays = [ inputs.mac-overlay.overlays.pgadmin4-mac ];
-    environment.systemPackages = [ pgadmin ];
+    environment.systemPackages = [ pgadmin4 ];
   };
 }
