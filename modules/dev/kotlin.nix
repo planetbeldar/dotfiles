67fce1071e1a4e373fea54f3b99459129458ee2d
Tony Höglund <tony.hoglund@gmail.com>
2025-02-05 09:38:39 +0100

    kotlin: add -Xmx option to module

--- modules/dev/kotlin.nix
+++ modules/dev/kotlin.nix
@@ -1,13 +1,14 @@
 { lib, pkgs, config, inputs, ... }:
 let 
-  inherit (lib) mkIf mkEnableOption;
+  inherit (lib) util types mkIf mkEnableOption;
   inherit (pkgs) fetchFromGitHub kotlin kotlin-language-server gradle ktlint;
 
   jdk = pkgs.openjdk21;
   cfg = config.modules.dev.kotlin;
 in {
   options.modules.dev.kotlin = {
-     enable = mkEnableOption "enable kotlin";
+     enable = mkEnableOption "kotlin";
+     xmx    = util.mkOpt types.str "8g";
   };
 
   config = mkIf cfg.enable {
@@ -21,7 +22,7 @@ in {
 
     env = {
       JAVA_HOME = "${jdk}";
-      JAVA_OPTS = "-Xmx16g";
+      JAVA_OPTS = "-Xmx${cfg.xmx}";
     };
   };
 }
